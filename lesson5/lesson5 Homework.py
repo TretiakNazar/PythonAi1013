ba8be85b6787eaa35f392627e11476a156043214
User <war.thander300@gmail.com>
2023-11-08 20:06:45 +0200

    Lesson5 homework

---
+++ lesson5/lesson5 Homework.py
@@ -0,0 +1,9 @@
+from colorama import Fore, Back, Style
+# Fore - задає колір тексту в консолі
+print(Fore.RED + "Red text")
+# Back - задає задній колір
+print(Back.GREEN + "Green text")
+print(Style.DIM + "Dim text")
+# Style.RESET_ALL - зкидає всі налаштування
+print(Style.RESET_ALL)
+print("somw text")
