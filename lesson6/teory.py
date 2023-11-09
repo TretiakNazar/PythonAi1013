9baf0d16a32d0eb405ea076fe79809858326e0df
User <war.thander300@gmail.com>
2023-11-09 16:00:30 +0200

    lesson6

---
+++ lesson6/teory.py
@@ -0,0 +1,19 @@
+from colorama import Fore, Back, Style
+try:
+    #a = 5 / 0
+    #int("hello")
+    a = int(input("Введіть ваш рахунок"))
+except ZeroDivisionError:
+    print(Fore.RED + "Помилка ділення на 0")
+except ValueError:
+    print(Fore.RED + "Помилка значення")
+except :
+    print("Помилка")
+else:
+    print(a ** 2)
+finally:
+    print(Fore.GREEN+"Кінець відловлення помилок")
+
+
+
+print(Style.RESET_ALL+"Кінець програми")
