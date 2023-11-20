78b7f2a02213f58bcb927c5082df72a6c0145de3
User <war.thander300@gmail.com>
2023-11-20 19:18:35 +0200

    lesson7 Homework

--- lesson7/lesson7 Homework.py
+++ lesson7/lesson7 Homework.py
@@ -1,8 +1,8 @@
 def raise_to_the_degrees(number, max_degree):
-    i = 0
+    n = 0
     for _ in range(max_degree):
-        yield number * i
-        i += 1
+        yield number * n
+        n += 1
 res = raise_to_the_degrees(2, 250)
 print(res)
 for _ in res:
